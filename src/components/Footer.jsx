8ce453a6d0150a4c30c02c0411e99f2b372ff8c7
Marcos Schloymann <marcosschloymann@gmail.com>
2024-04-14 20:02:24 -0300

    14-4-24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer'>
-      <h4>+5493731626149</h4>
+      {/* <h4>+5493731626149</h4>
       <h4>
         mideulcecata@gmail.com
       </h4>
       <h4>
         El Chocolate de las Reinas
-      </h4>
+      </h4> */}
       <span>
         get-it
       </span>
